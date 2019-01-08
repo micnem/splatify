3d9b6da4dac2f9a027bfd540c5a656d787bf3774
micnem <michael.nemni@gmail.com>
2019-01-08 23:04:30 +0200

    Add files via upload

---
+++ Intersect/Intersect/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Intersect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list1 = new List<int>();
+            List<int> list2 = new List<int>();
+            List<int> list3 = new List<int>();
+
+            list1.AddRange(new int[] { 1, 2, 4, 5, 6, 9, 10 });
+            list2.AddRange(new int[] { 1, 2, 5, 7, 8, 10, 11 });
+            list3.AddRange(new int[] { 2, 3, 6, 7, 9, 10 });
+
+            IEnumerable<int> listCommon = list1.Intersect(list2).Intersect(list3).ToArray();
+
+            foreach (int i in listCommon)
+            {
+                Console.Write(i + " ");
+            }
+
+        }
+    }
+}
---
+++ Test/Test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Operators
+{
+    class LINQQueryExpressions
+    {
+        static void Main()
+        {
+
+            // Specify the data source.
+            int[] scores = new int[] { 97, 92, 81, 60 };
+
+            // Define the query expression.
+            IEnumerable<int> scoreQuery = from score in scores where score > 90 select score;
+
+            // Execute the query.
+
+            foreach (int i in scoreQuery)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
